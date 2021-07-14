22584b57de2713631c10833cfd0e7301a0a5d38b
Kalash <kalash.strt@gmail.com>
2021-07-14 23:38:32 +0530

    getPlanet is functioning now

--- controllers/getPlanet.js
+++ controllers/getPlanet.js
@@ -1,3 +1,29 @@
+// planet information
+const planetInformation = {
+  mercury: {
+    test: "mercury",
+  },
+
+  venus: {
+    test: "venus",
+  },
+};
+
+// getPlanet route callback function
 exports.getPlanetController = (req, res) => {
-  res.send("Planet is send");
+  const { name } = req.query;
+
+  let refactored_name = name.trim(); // removing any trailing white spaces
+  refactored_name = refactored_name.toLowerCase(); // converting to lowercase letters
+
+  // getting planet data
+  const planetData = planetInformation[refactored_name];
+
+  if (planetData) {
+    res.statusCode = 200;
+    res.send(planetData);
+  } else {
+    res.statusCode = 404;
+    res.send({ error: "Not found!" });
+  }
 };
